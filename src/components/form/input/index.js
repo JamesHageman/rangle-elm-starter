2b735797527a23487ad76b7a02add98c43f13542
Seth Davenport <SethDavenport@users.noreply.github.com>
2016-07-09 20:19:04 -0400

    Upgrade to react 15.2.1 (#227)
    * Upgrade to react 15.2.1
    
    * Upgrade to react 15.2.1
    
    * adjust circle artifacts

--- src/components/form/input/index.js
+++ src/components/form/input/index.js
@@ -3,23 +3,34 @@ import React from 'react';
 function Input({
   type = 'text',
   placeholder = '',
-  fieldDefinition,
-  ...props,
+  fieldDefinition = {},
+  id,
 }) {
+  const {
+    value,
+    onBlur,
+    onChange,
+    onFocus,
+  } = fieldDefinition;
+
   return (
     <input
+      id={ id }
       className="block col-12 mb1 input"
       type={ type }
       placeholder={ placeholder }
-      { ...fieldDefinition }
-      { ...props } />
+      value={ value }
+      onBlur={ onBlur }
+      onChange={ onChange }
+      onFocus={ onFocus } />
   );
 }
 
 Input.propTypes = {
   type: React.PropTypes.string,
   placeholder: React.PropTypes.string,
-  fieldDefinition: React.PropTypes.object.isRequired,
+  fieldDefinition: React.PropTypes.object,
+  id: React.PropTypes.string,
 };
 
 export default Input;
